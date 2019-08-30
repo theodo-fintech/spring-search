abe80c4131c8949f93ac05374b5236e72dfcf5fb
Thibault F <thibaultf@sipios.com>
2019-08-30 17:51:10 +0200

    fix spelling mistakes

--- src/main/kotlin/com/sipios/springsearch/SearchCriteria.kt
+++ src/main/kotlin/com/sipios/springsearch/SearchCriteria.kt
@@ -12,25 +12,25 @@ class SearchCriteria// Change EQUALS into ENDS_WITH, CONTAINS, STARTS_WITH based
         var op = SearchOperation.getSimpleOperation(operation[0])
         if (op != null) {
             // Change EQUALS into ENDS_WITH, CONTAINS, STARTS_WITH based on the presence of * in the value
-            val startWithAsterisk = prefix != null && prefix.contains(SearchOperation.ZERO_OR_MORE_REGEX)
-            val endWithAsterisk = suffix != null && suffix.contains(SearchOperation.ZERO_OR_MORE_REGEX)
-            if (op === SearchOperation.EQUALS && startWithAsterisk && endWithAsterisk) {
+            val startsWithAsterisk = prefix != null && prefix.contains(SearchOperation.ZERO_OR_MORE_REGEX)
+            val endsWithAsterisk = suffix != null && suffix.contains(SearchOperation.ZERO_OR_MORE_REGEX)
+            if (op === SearchOperation.EQUALS && startsWithAsterisk && endsWithAsterisk) {
                 op = SearchOperation.CONTAINS
-            } else if (op === SearchOperation.EQUALS && startWithAsterisk) {
+            } else if (op === SearchOperation.EQUALS && startsWithAsterisk) {
                 op = SearchOperation.ENDS_WITH
-            } else if (op === SearchOperation.EQUALS && endWithAsterisk) {
+            } else if (op === SearchOperation.EQUALS && endsWithAsterisk) {
                 op = SearchOperation.STARTS_WITH
             }
-            // Change NOT_EQUALS into DOESNT_ENDS_WITH, DOESNT_CONTAINS, DOESNT_STARTS_WITH based on the presence of * in the value
-            if (op === SearchOperation.NOT_EQUALS && startWithAsterisk && endWithAsterisk) {
+            // Change NOT_EQUALS into DOESNT_END_WITH, DOESNT_CONTAIN, DOESNT_START_WITH based on the presence of * in the value
+            if (op === SearchOperation.NOT_EQUALS && startsWithAsterisk && endsWithAsterisk) {
                 op = SearchOperation.DOESNT_CONTAIN
-            } else if (op === SearchOperation.NOT_EQUALS && startWithAsterisk) {
+            } else if (op === SearchOperation.NOT_EQUALS && startsWithAsterisk) {
                 op = SearchOperation.DOESNT_END_WITH
-            } else if (op === SearchOperation.NOT_EQUALS && endWithAsterisk) {
+            } else if (op === SearchOperation.NOT_EQUALS && endsWithAsterisk) {
                 op = SearchOperation.DOESNT_START_WITH
             }
         }
         this.operation = op
-        this.value = URLDecoder.decode(value,"UTF-8")
+        this.value = URLDecoder.decode(value, "UTF-8")
     }
 }
