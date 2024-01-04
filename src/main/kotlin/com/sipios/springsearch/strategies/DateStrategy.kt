2ddcc64dee1f96c99c12ccd264b7e633f9032298
vincentescoffier <vincent.escoffier1@gmail.com>
2024-01-04 11:03:00 +0100

    update strategies and add tests

--- src/main/kotlin/com/sipios/springsearch/strategies/DateStrategy.kt
+++ src/main/kotlin/com/sipios/springsearch/strategies/DateStrategy.kt
@@ -22,6 +22,8 @@ class DateStrategy : ParsingStrategy {
         return when (ops) {
             SearchOperation.GREATER_THAN -> builder.greaterThan(path[fieldName], value as Date)
             SearchOperation.LESS_THAN -> builder.lessThan(path[fieldName], value as Date)
+            SearchOperation.GREATER_THAN_EQUALS -> builder.greaterThanOrEqualTo(path[fieldName], value as Date)
+            SearchOperation.LESS_THAN_EQUALS -> builder.lessThanOrEqualTo(path[fieldName], value as Date)
             else -> super.buildPredicate(builder, path, fieldName, ops, value)
         }
     }
