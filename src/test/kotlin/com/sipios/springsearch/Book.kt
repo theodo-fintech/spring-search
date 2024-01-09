972632c254258935e0573d298b7143b564cdadc0
vincentescoffier <vincent.escoffier1@gmail.com>
2024-01-09 15:11:13 +0100

    fix: formatting

--- src/test/kotlin/com/sipios/springsearch/Book.kt
+++ src/test/kotlin/com/sipios/springsearch/Book.kt
@@ -1,6 +1,13 @@
 package com.sipios.springsearch
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import org.hibernate.proxy.HibernateProxy
 
 @Entity
