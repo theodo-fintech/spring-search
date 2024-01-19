9f4d68c9fd07556ebca84c40bca8fbf0ec177534
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2024-01-19 15:08:16 +0100

    refactor: remove unnecessary non null assertions and use grouped regex

--- src/main/kotlin/com/sipios/springsearch/QueryVisitorImpl.kt
+++ src/main/kotlin/com/sipios/springsearch/QueryVisitorImpl.kt
@@ -6,7 +6,7 @@ import com.sipios.springsearch.grammar.QueryParser
 import org.springframework.data.jpa.domain.Specification
 
 class QueryVisitorImpl<T>(private val searchSpecAnnotation: SearchSpec) : QueryBaseVisitor<Specification<T>>() {
-    private val valueRegExp = Regex(pattern = "^(\\*?)(.+?)(\\*?)$")
+    private val valueRegExp = Regex(pattern = "^(?<prefix>\\*?)(?<value>.+?)(?<suffix>\\*?)$")
     override fun visitOpQuery(ctx: QueryParser.OpQueryContext): Specification<T> {
         val left = visit(ctx.left)
         val right = visit(ctx.right)
@@ -31,7 +31,7 @@ class QueryVisitorImpl<T>(private val searchSpecAnnotation: SearchSpec) : QueryB
     }
 
     override fun visitIsCriteria(ctx: QueryParser.IsCriteriaContext): Specification<T> {
-        val key = ctx.key()!!.text
+        val key = ctx.key().text
         val op = if (ctx.IS() != null) {
             SearchOperation.IS
         } else {
@@ -42,7 +42,7 @@ class QueryVisitorImpl<T>(private val searchSpecAnnotation: SearchSpec) : QueryB
     }
 
     override fun visitEqArrayCriteria(ctx: QueryParser.EqArrayCriteriaContext): Specification<T> {
-        val key = ctx.key()!!.text
+        val key = ctx.key().text
         val op = if (ctx.IN() != null) {
             SearchOperation.IN_ARRAY
         } else {
@@ -58,7 +58,7 @@ class QueryVisitorImpl<T>(private val searchSpecAnnotation: SearchSpec) : QueryB
     }
 
     override fun visitBetweenCriteria(ctx: QueryParser.BetweenCriteriaContext): Specification<T> {
-        val key = ctx.key()!!.text
+        val key = ctx.key().text
         val leftValue = if (ctx.left.STRING() != null) {
             clearString(ctx.left.text)
         } else {
@@ -88,19 +88,19 @@ class QueryVisitorImpl<T>(private val searchSpecAnnotation: SearchSpec) : QueryB
     }
 
     override fun visitOpCriteria(ctx: QueryParser.OpCriteriaContext): Specification<T> {
-        val key = ctx.key()!!.text
-        var value = ctx.value()!!.text
+        val key = ctx.key().text
+        var value = ctx.value().text
         if (ctx.value().STRING() != null) {
             value = clearString(value)
         }
-        val matchResult = this.valueRegExp.find(value!!)
-        val op = SearchOperation.getSimpleOperation(ctx.op().text)!!
+        val matchResult = this.valueRegExp.find(value)
+        val op = SearchOperation.getSimpleOperation(ctx.op().text) ?: throw IllegalArgumentException("Invalid operation")
         val criteria = SearchCriteria(
             key,
             op,
-            matchResult!!.groups[1]!!.value,
-            matchResult.groups[2]!!.value,
-            matchResult.groups[3]!!.value
+            matchResult!!.groups["prefix"]!!.value,
+            matchResult.groups["value"]!!.value,
+            matchResult.groups["suffix"]!!.value
         )
 
         return SpecificationImpl(criteria, searchSpecAnnotation)
